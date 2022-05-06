56754dc37858518fdd4bfe8cb06dbcd894d66450
Tony Di Benedetto <tonyblast@gmail.com>
2022-05-06 08:56:33 +0200

    ci: use docker compose instead of docker-compose

--- docker/test-integration/run.sh
+++ docker/test-integration/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker-compose -p nestjs-api build
-DOCKER_BUILDKIT=1 docker-compose -p nestjs-api up --force-recreate --renew-anon-volumes --abort-on-container-exit && \
+DOCKER_BUILDKIT=1 docker compose -p nestjs-api build
+DOCKER_BUILDKIT=1 docker compose -p nestjs-api up --force-recreate --renew-anon-volumes --abort-on-container-exit && \
 
 CODE=0
-docker-compose -p nestjs-api ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do
+docker compose -p nestjs-api ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do
     if [ "$code" == "1" ]; then
        CODE=-1
     fi
 done
-docker-compose -p nestjs-api down -v
+docker compose -p nestjs-api down -v
 exit $CODE
 
